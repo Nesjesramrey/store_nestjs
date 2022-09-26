aa5bf25e595a0dbaa673b613bf578213ecbf0d9a
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-09-26 18:30:10 -0500

    add dtos

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,22 @@
+import { IsString, IsNumber, IsUrl, IsNotEmpty } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsNumber()
   readonly price: number;
+
+  @IsNumber()
   readonly stock: number;
+
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
